e373948bbbb89caac0fd5f09f1e880af7cb6c7ba
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-16 18:39:26 -0500

    consultar empleado por id

--- report-server/src/basic-reports/basic-reports.controller.ts
+++ report-server/src/basic-reports/basic-reports.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { BasicReportsService } from './basic-reports.service';
 import { response, Response } from 'express';
 
@@ -16,11 +16,23 @@ export class BasicReportsController {
   }
 
   @Get('/employment-letter')
-  async employmentLetter(@Res() response:Response){
-    const pdfDoc=this.basicReportsService.employmentLetter()
-    response.setHeader('Content-Type',"application/pdf")
-    pdfDoc.info.Title="Recomendación.pdf"
-    pdfDoc.pipe(response)
-    pdfDoc.end()
+  async employmentLetter(@Res() response: Response) {
+    const pdfDoc = this.basicReportsService.employmentLetter();
+    response.setHeader('Content-Type', 'application/pdf');
+    pdfDoc.info.Title = 'Recomendación.pdf';
+    pdfDoc.pipe(response);
+    pdfDoc.end();
+  }
+
+  @Get('/employment-letter/:employeeId')
+  async employmentLetterById(
+    @Res() response: Response,
+    @Param('employeeId') employeeId: string,
+  ) {
+    const pdfDoc = await this.basicReportsService.employmentLetterById(+employeeId);
+    response.setHeader('Content-Type', 'application/pdf');
+    pdfDoc.info.Title = 'Recomendación.pdf';
+    pdfDoc.pipe(response);
+    pdfDoc.end();
   }
 }
--- report-server/src/basic-reports/basic-reports.service.ts
+++ report-server/src/basic-reports/basic-reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrinterService } from 'src/printer/printer.service';
 import { getEmploymentLetterReport, getHelloWorldReport } from 'src/reports';
@@ -19,6 +19,17 @@ export class BasicReportsService extends PrismaClient implements OnModuleInit {
   }
 
   employmentLetter() {
+    const docDefinition = getEmploymentLetterReport();
+    return this.printerService.createPdf(docDefinition);
+  }
+
+  async employmentLetterById(employeeId: number) {
+    const employee = await this.employees.findUnique({
+      where: { id: employeeId },
+    });
+    if (!employee) {
+      throw new NotFoundException(`Empleado con ${employeeId} no encontrado`);
+    }
     const docDefinition = getEmploymentLetterReport();
     return this.printerService.createPdf(docDefinition);
   }
