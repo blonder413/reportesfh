bfb0f6d6852d098bd3ddc4086cfb89e2374b8735
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-20 11:19:21 -0500

    estilo en la misma línea

--- report-server/src/reports/order-by-id.report.ts
+++ report-server/src/reports/order-by-id.report.ts
@@ -4,6 +4,7 @@ import {
   TDocumentDefinitions,
 } from 'pdfmake/interfaces';
 import { DateFormatter } from 'src/helpers';
+import { footerSection } from './sections/footer.section';
 
 const logo: Content = {
   image: 'src/assets/logo.png',
@@ -18,6 +19,11 @@ const styles: StyleDictionary = {
     fontSize: 20,
     margin: [0, 30, 0, 0],
   },
+  subheader: {
+    fontSize: 16,
+    bold: true,
+    margin: [0, 20, 0, 0],
+  },
 };
 
 export const orderByIdReport = (): TDocumentDefinitions => {
@@ -28,16 +34,28 @@ export const orderByIdReport = (): TDocumentDefinitions => {
       // dirección
       {
         columns: [
-          { text: 'Raccoon City\nUSA' },
+          { text: 'Raccoon City\nUSA\nblonder413.wordpress.com' },
           {
             alignment: 'right',
-            text: `Recibo No. 413\nFecha del recibo: ${DateFormatter.getHumanDate()}\nPagar antes de: ${DateFormatter.getHumanDate()}\n`,
+            text: [
+              { text: `Recibo No. 413\n`, bold: true },
+              `Fecha del recibo: ${DateFormatter.getHumanDate()}\nPagar antes de: ${DateFormatter.getHumanDate()}\n`,
+            ],
           },
         ],
       },
       // qr
       { alignment: 'right', qr: 'https://blonder413.wordpress.com', fit: 75 },
+      // dirección del cliente
+      {
+        text: [
+          { text: 'Cobrar a:', style: 'subHeader' },
+          `\nRazón social: BSAA org,
+          USA`,
+        ],
+      },
     ],
+    footer:footerSection,
     header: logo,
     pageMargins: [40, 60, 40, 60],
     styles: styles,
