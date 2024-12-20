fbaf138d1cd2603d6ae1acb46dc133bf1f125916
Jonathan Morales <jonathan.moralessalazar@externos-cl.cencosud.com>
2024-12-20 15:04:02 -0500

    información completa del recibo

--- report-server/src/reports/order-by-id.report.ts
+++ report-server/src/reports/order-by-id.report.ts
@@ -26,7 +26,49 @@ const styles: StyleDictionary = {
   },
 };
 
-export const orderByIdReport = (): TDocumentDefinitions => {
+export interface Customers {
+  customer_id: number;
+  customer_name: string;
+  contact_name: string;
+  address: string;
+  city: string;
+  postal_code: string;
+  country: string;
+}
+
+export interface Products {
+  product_id: number;
+  product_name: string;
+  category_id: number;
+  unit: string;
+  price: string;
+}
+
+export interface OrderDetail {
+  order_detail_id: number;
+  order_id: number;
+  quantity: number;
+  products: Products;
+}
+
+export interface CompleteOrder {
+  order_id: number;
+  customer_id: number;
+  order_date: Date;
+  customers: Customers;
+  order_details: OrderDetail[];
+}
+
+interface ReportValues {
+  title?: string;
+  subtitle?: string;
+  data: CompleteOrder;
+}
+
+export const orderByIdReport = (value: ReportValues): TDocumentDefinitions => {
+  const { data } = value;
+  /* console.log(data); */
+
   return {
     content: [
       // Headers
--- report-server/src/store-reports/store-reports.service.ts
+++ report-server/src/store-reports/store-reports.service.ts
@@ -1,13 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import { PrinterService } from 'src/printer/printer.service';
 import { orderByIdReport } from 'src/reports';
 
 @Injectable()
-export class StoreReportsService {
-  constructor(private readonly printerService: PrinterService) {}
+export class StoreReportsService extends PrismaClient implements OnModuleInit {
+  constructor(private readonly printerService: PrinterService) {
+    super();
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
 
   async getOrderByIdReport(orderId: number) {
-    const docDefinition = orderByIdReport();
+    const order = await this.orders.findUnique({
+      where: { order_id: orderId },
+      include: {
+        customers: true,
+        order_details: { include: { products: true } },
+      },
+    });
+    if (!order) {
+      throw new NotFoundException(`Order con el id ${orderId} no existe`);
+    }
+    /* console.log(JSON.stringify(order)); */
+
+    const docDefinition = orderByIdReport({ data: order as any });
     return this.printerService.createPdf(docDefinition);
   }
 }
