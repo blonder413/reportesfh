9c7310c33bd6ba4fcc6ba9c484821078d9c4c101
Jonathan Morales <blonder413@gmail.com>
2024-12-21 14:39:07 -0500

    parámetros adicionales - QuickChart

--- report-server/src/helpers/chart-utils.ts
+++ report-server/src/helpers/chart-utils.ts
@@ -1,8 +1,19 @@
 import axios from 'axios';
 
-export const chartJsToImage = async (chartConfig: unknown) => {
+interface ChartOptions {
+  height?: number;
+  width?: number;
+}
+
+export const chartJsToImage = async (
+  chartConfig: unknown,
+  options: ChartOptions = {},
+) => {
+  const params = new URLSearchParams();
+  if (options.height) params.append('height', options.height.toString());
+  if (options.width) params.append('width', options.width.toString());
   const encoderUri = encodeURIComponent(JSON.stringify(chartConfig));
-  const chartUrl = `https://quickchart.io/chart?c=${encoderUri}`;
+  const chartUrl = `https://quickchart.io/chart?c=${encoderUri}&${params.toString()}`;
   const response = await axios.get(chartUrl, { responseType: 'arraybuffer' });
   return `data:image/png;base64,${Buffer.from(response.data).toString('base64')}`;
 };
